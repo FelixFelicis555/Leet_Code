8644faa93644ed47d6f6bad46a93cde4275b3dce
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-09-08 10:40:37 +0530

    Time: 2536 ms (37.64%), Space: 14 MB (81.43%) - LeetHub

---
+++ 935-knight-dialer/935-knight-dialer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        moves=[[4,6],[6,8],[7,9],[4,8],[0,3,9],[],[0,1,7],[2,6],[1,3],[2,4]]
+        result=[1]*10
+        hops=0
+        while hops<n-1:
+            nextResult=[0]*10
+            for idx in range(len(result)):
+                for x in moves[idx]:
+                    nextResult[idx]+=result[x]
+            result=nextResult
+            hops+=1
+        return sum(result)%(10**9+7)    
+            
