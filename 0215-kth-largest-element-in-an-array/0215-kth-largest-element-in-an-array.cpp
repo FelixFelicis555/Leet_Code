e7dded074ec6bcdddd644f15a95e3e891f5f45b4
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-10-25 10:28:01 +0530

    Time: 329 ms (11.75%), Space: 47.4 MB (31.89%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,7 +1,46 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end(),greater<int>());
+    
+    /*
+    Approach-1: Sorting 
+    
+     sort(nums.begin(),nums.end(),greater<int>());
         return nums[k-1];
+        
+        Approach-2 : Heap
+        
+        O(nlogn)
+        
+        Whenever there's need to use sorting,we can try to use heap
+        
+        Optimal solution: MIn-heap
+        TC-O(nlogk)
+        SC-O(k)
+        
+        Whenever we see kth largest,max,the optimal solution is to think of using min-heap
+        
+        We store only k-elements always
+        In min-heap ,top-most element will be min-value
+        
+        
+    
+    */
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(pq.size()<k)
+                pq.push(nums[i]);
+            else
+            {
+                if(pq.top()<nums[i])
+                {
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
+        }
+        return pq.top();
     }
 };
