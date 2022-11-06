4b06b184c000c8a17a11ecd50ec4d9ef2cf5612e
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-11-06 12:45:03 +0530

    Time: 32 ms (75.69%), Space: 10.7 MB (50.23%) - LeetHub

---
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        unordered_map<int,long>m;
+        int mod=1e9+7;
+        for(auto x:arr)
+            m[x]++;
+        long res=0;
+        for(auto it:m)
+        {
+            for(auto it2:m)
+            {
+                int i=it.first,j=it2.first,k=target-i-j;
+                if(!m.count(k))
+                    continue;
+                if(i==j && j==k)
+                    res+=m[i]*(m[i]-1)*(m[i]-2) / 6;
+                else if(i==j && j!=k)
+                    res+=m[i]*(m[i]-1)/2 *m[k];
+                else if(i<j && j<k)
+                    res+=m[i]*m[j]*m[k];
+            }
+        }
+        return res%mod;
+    }
+};
