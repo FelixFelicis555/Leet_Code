73596e14af64db9166aa8d54a71de74d74fb9280
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-11-06 19:17:09 +0530

    Time: 3 ms (84.36%), Space: 8.4 MB (97.61%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(),citations.end());
+      int ans=0;
+        for(int i=citations.size()-1;i>=0;i--)
+        {
+            if(citations[i]>=citations.size()-i)
+            {
+                ans++;
+            }
+        }
+        return ans;
+        
+    }
+};
