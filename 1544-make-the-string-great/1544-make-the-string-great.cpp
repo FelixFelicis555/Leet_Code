427b0c07cbbe5391f3cc8f538711bbdf11e83011
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-11-08 10:15:59 +0530

    Time: 11 ms (14.85%), Space: 6.3 MB (82.16%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string res;
+        for(char ch:s)
+        {
+            if(!res.empty() && abs(res.back()-ch)==32)
+                res.pop_back();
+            else
+                res.push_back(ch);
+        }
+        return res;
+    }
+};
