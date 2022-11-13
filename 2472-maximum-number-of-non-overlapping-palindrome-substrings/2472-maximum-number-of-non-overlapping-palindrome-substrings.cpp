c6d314f02c8466b7516f2b03472c9a604e2573ae
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-11-14 00:13:01 +0530

    Time: 19 ms (77.78%), Space: 10.9 MB (33.33%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool pal[2002][2002];
+    int maxPalindromes(string s, int k) {
+        if(k==1)
+            return s.size();
+        for(int len=1;len<=k+1;len++)
+        {
+           for(int i=0,j=i+len-1;j<s.size();i++,j++)
+           {
+               pal[i][j]=(len<3?true:pal[i+1][j-1]) && (s[i]==s[j]);
+           }
+        }
+        int i=0,res=0;
+        for(int i=0;i+k<=s.size();i++)
+        {
+            res+=pal[i][i+k-1] || pal[i][i+k];
+            i+=pal[i][i+k-1]? k-1:pal[i][i+k]?k:0;
+        }
+        return res;
+    }
+};
