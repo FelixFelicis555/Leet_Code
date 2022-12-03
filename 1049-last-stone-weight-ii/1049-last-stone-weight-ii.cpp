13263aba50b89184ae2b8f7aecdfb9e49cd7b562
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-12-03 23:35:46 +0530

    Time: 2 ms (97.40%), Space: 8.1 MB (86.49%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+       int n=stones.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+            sum+=stones[i];
+        int s=sum/2;
+        int ans=INT_MAX;
+        bool t[n+1][s+1];
+        
+        
+        for(int i=0;i<n+1;i++)
+            t[i][0]=true;
+        for(int i=1;i<s+1;i++)
+            t[0][i]=false;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<s+1;j++){
+                if(stones[i-1]<=j){
+                    t[i][j]=t[i-1][j-stones[i-1]] || t[i-1][j];
+                }
+                else
+                    t[i][j]=t[i-1][j];
+            }
+        }
+        
+        int maxi=0;
+        for(int i=1;i<s+1;i++)
+        {
+         if(t[n][i] == true)
+       	   
+            maxi=max(maxi,i);
+        }
+        
+       
+        ans=min(ans, sum-(2*maxi));
+        return ans;  
+    }
+};
