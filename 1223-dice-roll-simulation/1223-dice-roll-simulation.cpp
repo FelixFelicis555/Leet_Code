cbaf03daab45d0eeaad5690b52799aa9e963aace
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-12-05 00:57:51 +0530

    Time: 97 ms (64.82%), Space: 9.9 MB (58.29%) - LeetHub

--- 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
+++ 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int dieSimulator(int n, vector<int>& rollMax) {
-         int t[n][6][16]; 
-       memset(t,0,sizeof(t));
-        
-       for(int i=0;i<6;i++)
-           t[0][i][1]=1;
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<6;j++){
-                for(int k=0;k<6;k++){
-                    if(k==j)
-                        continue;
-                    for(int m=1;m<=rollMax[k];m++){
-                        t[i][j][1]=(t[i][j][1]+t[i-1][k][m])%1000000007;
-                    }
-                   
-                    
-                }
-                 for(int m=2;m<=rollMax[j];m++){
-                        t[i][j][m]=(t[i-1][j][m-1])%1000000007;
-                    }
+        long p = 1e9 + 7;
+        vector<vector<long>> dp(n+1, vector<long>(6, 0));
+        vector<long> sum(n+1, 0);
+        sum[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < 6; ++j)  {
+                for (int k = 1; k <= rollMax[j] && i - k >= 0; ++k)
+                    dp[i][j] = (dp[i][j] + sum[i-k] - dp[i-k][j] + p) % p;
+                sum[i] = (sum[i] + dp[i][j]) % p;
             }
         }
-        
-        int ans=0;
-        
-        for(int i=0;i<6;i++){
-            for(int j=1;j<=rollMax[i];j++){
-                ans=(ans+t[n-1][i][j])%1000000007;
-            }
-        }
-        return ans;
+
+        return sum[n]; 
     }
 };
