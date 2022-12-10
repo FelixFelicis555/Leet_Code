921ad6fbade523b9a46643c9f9cf5bea3a8064a4
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-12-11 01:12:12 +0530

    Time: 467 ms (33.33%), Space: 65.4 MB (55.56%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int maxJump(vector<int>& stones) {
-        vector<int>st(stones);
+        
+       /* vector<int>st(stones);
         return max(st[1] - st[0], transform_reduce(
         begin(st) + 2, end(st), begin(st), 0, 
         [](int a, int b){ return max(a, b); },
         [](int i, int j){ return i - j; }));
+        
+        */
+        int res=stones[1]-stones[0];
+        for(int i=1;i<stones.size()-1;i++)
+        {
+            res=max(res,stones[i+1]-stones[i-1]);
+        }
+        return res;
     }
 };
