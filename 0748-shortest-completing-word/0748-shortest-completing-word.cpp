2d2866c1efec25cb7781273e809ef285fa7434f5
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2022-12-12 16:06:56 +0530

    Time: 28 ms (69.31%), Space: 11.7 MB (96.68%) - LeetHub

---
+++ 0748-shortest-completing-word/0748-shortest-completing-word.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+         vector<int> m(26);
+        string ans="";
+        for(int i=0;i<licensePlate.size();i++){
+            if(licensePlate[i]>='a' && licensePlate[i]<='z')
+                m[licensePlate[i]-'a']++;
+            else if(licensePlate[i]>='A' && licensePlate[i]<='Z')
+                m[tolower(licensePlate[i])-'a']++;
+        }
+        vector<int> n(26);
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words[i].size();j++){
+                n[words[i][j]-'a']++;
+            }
+            bool f=true;
+            for(int i=0;i<26;i++){
+                if(m[i]>n[i]){
+                    f=false;
+                    break;
+                }
+            }
+            if(f==true){
+                if(ans.size()>words[i].size() || ans=="")
+                   ans=words[i]; 
+            }
+            for(int i=0;i<26;i++)
+                n[i]=0;
+        }
+        return ans;
+    }
+};
