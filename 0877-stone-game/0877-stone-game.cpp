1ce0f91bbd5e9816dbd286681be8efdf7088b772
Pavanesh <78358679+FelixFelicis555@users.noreply.github.com>
2023-01-03 22:30:04 +0530

    Time: 3 ms (78.87%), Space: 7.5 MB (65.30%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-       return true; 
+    int dp[501][501];
+    bool help(int i,int j,vector<int>&p)
+    {
+        if(i>j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        return dp[i][j]=max(p[i]-help(i+1,j,p),p[j]-help(i,j-1,p));
+    }
+    bool stoneGame(vector<int>& p) {
+       return true;
     }
 };
